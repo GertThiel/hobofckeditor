6905799b4c22e619df2c31406382eedb56cb757a
Gert Thiel <GertThiel@gmx.net>
2008-10-26 08:16:41 +0100

    Added Separate fckconfig.js To Configure FCKeditor

--- public/javascripts/hobofckeditor.js
+++ public/javascripts/hobofckeditor.js
@@ -2,6 +2,7 @@ Event.addBehavior({
     'textarea.html' : function() {
         var ed = new FCKeditor( this.id );
         ed.BasePath = "/javascripts/fckeditor/";
+        ed.Config["CustomConfigurationsPath"] = "/javascripts/fckconfig.js";
         ed.ReplaceTextarea();
     }
 })
